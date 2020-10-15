81a35947335314ea991da737118819d14d303445
aleksandarm03 <accam003@gmail.com>
2020-10-15 18:58:00 +0200

    update:15.10.2020

---
+++ TV/TV/Form1.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace WindowsFormsApplication1
+{
+    
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        tv[] t = new tv[100];
+        int n = 0;
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            textBox2.Clear();
+
+            try
+            {
+                StreamReader f = new StreamReader("tv.txt");
+                n = 0;
+                while (!f.EndOfStream)
+                {
+                    string p = f.ReadLine();
+                    string m = f.ReadLine();
+                    double c = Convert.ToDouble(f.ReadLine());
+                    t[n] = new tv(p, m, c);
+                    n++;
+                }
+                f.Close();
+
+                for (int i = 0; i < n; i++)
+                {
+                    for (int j = i + 1; j < n; j++)
+                    {
+                        if (t[j].Cena < t[i].Cena)
+                        {
+                            tv pom = t[i];
+                            t[i] = t[j];
+                            t[j] = pom;
+                        }
+                    }
+                }
+
+
+                listBox1.Items.Clear();
+                for (int i = 0; i < n; i++)
+                {
+                    listBox1.Items.Add("Naziv: " + t[i].Naziv);
+                    listBox1.Items.Add("Model: " + t[i].Model);
+                    //ne moze t[i].cena, zato sto je private
+                    listBox1.Items.Add("Cena: " + t[i].Cena + "RSD");
+                    listBox1.Items.Add("* * *");
+                }
+                button2.Enabled = false;
+            }
+            catch (Exception izuzetak)
+            {
+                MessageBox.Show(izuzetak.Message);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                StreamReader fr = new StreamReader("tv.txt");
+                n = 0;
+                while (!fr.EndOfStream)
+                {
+                    string p = fr.ReadLine();
+                    string m = fr.ReadLine();
+                    double c = Convert.ToDouble(fr.ReadLine());
+                    t[n] = new tv(p, m, c);
+                    n++;
+                }
+                fr.Close();
+                double procenat = Convert.ToDouble(textBox2.Text);
+                for (int i = 0; i < n; i++)
+                {
+                    if (t[i].Naziv.Equals(textBox1.Text))
+                    {
+                        t[i].Cena = t[i].Cena * (100 - procenat) / 100;
+                    }
+                }
+                listBox1.Items.Clear();
+                StreamWriter fw = new StreamWriter("spisak.txt");
+                for (int i = 0; i < n; i++)
+                {
+                    fw.WriteLine(t[i].Naziv);
+                    fw.WriteLine(t[i].Model);
+                    fw.WriteLine(t[i].Cena.ToString());
+                }
+                fw.Close();
+
+
+
+
+                listBox1.Items.Clear();
+                for (int i = 0; i < n; i++)
+                {
+                    listBox1.Items.Add("Naziv: " + t[i].Naziv);
+                    listBox1.Items.Add("Model: " + t[i].Model);
+                    listBox1.Items.Add("Cena: " + t[i].Cena + "RSD");
+                    listBox1.Items.Add("* * *");
+                }
+                button2.Enabled = true;
+            }
+            catch (Exception izuzetak)
+            {
+                MessageBox.Show(izuzetak.Message);
+            }
+        }
+    }
+}
---
+++ TV/TV/tv.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+namespace WindowsFormsApplication1
+{
+    class tv
+    {
+        private string naziv, model;
+        private double cena;
+
+        public string Naziv
+        {
+            get { return naziv; }
+        }
+
+        public string Model
+        {
+            get { return model; }
+        }
+
+        public double Cena
+        {
+            get { return cena; }
+            set
+            {
+                if (value < 0) throw new Exception("Cena ne moze biti negativan broj");
+                else cena = value;
+            }
+        }
+
+        public tv()
+        {
+            naziv = model = "";
+            cena = 0;
+        }
+
+        public tv(string p, string m, double c)
+        {
+            if (p.Equals(String.Empty))
+                throw new Exception("Nema naziv");
+            if (m.Equals(String.Empty))
+                throw new Exception("nema model");
+            naziv = p;
+            model = m;
+            Cena = c;
+        }
+
+        public bool JeftinijiOd(tv t)
+        {
+            return cena < t.cena;
+        }
+
+        public static bool operator <(tv t1, tv t2)
+        {
+            return t1.cena < t2.cena;
+        }
+
+        public static bool operator >(tv t1, tv t2)
+        {
+            return t1.cena > t2.cena;
+        }
+
+        public override string ToString()
+        {
+            return naziv + " - " + model + " - " + cena + "RSD";
+        }
+
+
+
+
+        public void Prikaz(ListBox lb)
+        {
+            //lb.Items.Add(ToString());
+            lb.Items.Add("naziv: " + naziv);
+            lb.Items.Add("Model: " + model);
+            lb.Items.Add("Cena: " + cena + "RSG");
+            lb.Items.Add("* * *");
+        }
+
+        public void Unos(StreamReader f)
+        {
+            naziv = f.ReadLine();
+            model = f.ReadLine();
+            if (naziv.Equals(String.Empty))
+                throw new Exception("nema naziv");
+            if (model.Equals(String.Empty))
+                throw new Exception("nema model");
+            Cena = Convert.ToDouble(f.ReadLine()); //poziva set
+
+            /* ukoliko nema svojstvo:
+            double c = Convert.ToDouble(f.ReadLine());
+            if (c < 0)
+                throw new Exception("cena<0!");
+            cena = c;
+            */
+        }
+
+        public void Ispis(StreamWriter f)
+        {
+            f.WriteLine(naziv);
+            f.WriteLine(model);
+            f.WriteLine(cena);
+        }
+
+        public void Popust(double procenat)
+        {
+            cena = cena * (100 - procenat) / 100;
+        }
+    }
+
+
+}
